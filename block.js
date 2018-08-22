243df7bc2afc6505c3806b0ccfc68563b3a84ae3
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-08-22 20:48:45 +0200

    create block

---
+++ block.js
@@ -0,0 +1,16 @@
+import SHA256 from 'crypto-js/sha256'
+
+class Block{
+  constructor(timestamp, data, previousHash = '') {
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    const input = this.previousHash + this.timestamp + JSON.stringify(this.data);
+    return SHA256(input).toString();
+  }
+}
